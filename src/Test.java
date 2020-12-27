60a7ff857e40268070f7c48957ed0c62b61c1cf5
1800720137 <1208535013@qq.com>
2020-12-27 20:24:22 +0800

    1.把EXcel读取到Uesr数组中。2.把键盘输入的用户名密码和Uesr用户名密码进行比对（登入过程的比对）

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,31 @@
+import java.io.File;
 import java.util.Scanner;
-public  class  Test{
-    public  static void main(String[] args){
-        System.out.println("请输入用户名");
-        Scanner sc=new Scanner(System.in);
-        String username=sc.next();
-        System.out.println("请输入密码");
-        String passworld=sc.next();
 
+public  class  Test {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Sakura\\IdeaProjects\\CmdShop\\src\\Users.xlsx");
+        ReadExcel readExcel = new ReadExcel();
+        User users[] = readExcel.readExcel(file);
+
+        System.out.println("请输入用户名");
+
+        Scanner sc = new Scanner(System.in);
+        String username = sc.next();//阻塞方法
+        System.out.println("你输入的用户名为" + username);
+
+        System.out.println("请输入用密码");
+        String password = sc.next();
+        System.out.println("你输入的密码为" + password);
+
+
+
+        System.out.println(users.length);
+        for (User user : users) {
+            if (username.equals(user.getUsername()) && password.equals(user.getPassword())){
+                System.out.println("登入成功");
+            }
+        }
     }
 }
+
+
